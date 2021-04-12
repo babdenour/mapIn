591011116f311e14419d7b253deced696d776a8c
Julia Francais <julia.francais@rocketstud.io>
2021-04-12 18:59:44 +0200

    test live share: add first style Co-authored-by: abdebens <babdenour@users.noreply.github.com>

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
 	import EncardPub from '$lib/components/EncardPub.svelte';
 	import Header from '$lib/Header.svelte';
-	import '../app.scss';
 </script>
 
 <div class="encard encard-top">
-	<EncardPub/>
+	<EncardPub />
 </div>
 
 <div class="header">
-	<Header/>
+	<Header />
 </div>
 
 <main class="main">
@@ -17,30 +16,30 @@
 </main>
 
 <div class="encard encard-bottom">
-	<EncardPub/>
+	<EncardPub />
 </div>
 
-
 <style lang="scss">
-.main {
-	width: 100%;
-	height: 100%	;
-	text-align: center;
-	padding: 0;
-	margin: 0;
-};
-.encard {
-	margin: auto;
-	display: block;
-	position: relative;
-};
+	@import '../style/app.scss';
 
-.encard-top {
-	top: 0;
-};
+	.main {
+		width: 100%;
+		height: 100%;
+		text-align: center;
+		padding: 0;
+		margin: 0;
+	}
+	.encard {
+		margin: auto;
+		display: block;
+		position: relative;
+	}
 
-.encard-bottom {
-	bottom: 0;
-};
+	.encard-top {
+		top: 0;
+	}
 
+	.encard-bottom {
+		bottom: 0;
+	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,41 +2,37 @@
 </script>
 
 <h1>
-	<a href="/map"  >
-		me diriger
-	</a>
+	<a href="/map"> me diriger </a>
 </h1>
 
-
 <style lang="scss">
+	@import '../style/app.scss';
+
 	h1 {
-		color: #911c22;
-	};
+		color: $red;
+	}
 	a {
 		text-decoration: none;
 		background-color: #cee1f2;
-		color: #911c22;
+		color: $red;
 		border-radius: 0.5em;
 		border-color: #cd323c;
 		padding: 0.7rem;
-	
-	};
+	}
 	a:hover {
-		border: 3px solid #911c22;
-		background-color:#cd323c;
-		color: #cee1f2;  
-	};
-	
-	a:active {
-		background-color:#cee1f2;
-		color: #911c22;
-	};
+		border: 3px solid $red;
+		background-color: #cd323c;
+		color: #cee1f2;
+	}
 
+	a:active {
+		background-color: #cee1f2;
+		color: #911c22;
+	}
 
 	@media (min-width: 480px) {
 		h1 {
 			max-width: none;
-		};
-
-	};
+		}
+	}
 </style>
